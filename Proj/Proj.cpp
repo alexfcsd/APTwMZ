4834aa38e606b6025712c764b9d070fbb30a5168
unknown <alexbestfor@gmail.com>
2018-05-06 19:03:24 +0200

    initial commit

---
+++ Proj/Proj.cpp
@@ -0,0 +1,14 @@
+// Proj.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello, World!\n";
+	system("pause");
+	return 0;
+}
+
